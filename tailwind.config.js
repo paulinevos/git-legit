adc76d04eb9e437bd102919dd58a28dc69ab92e1
Pauline Vos <pv@daedalean.ai>
2024-06-26 22:08:59 +0200

    Initial landing page
    Including email sign-ups for the wait list.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./css/**/*.{html,js}","index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
